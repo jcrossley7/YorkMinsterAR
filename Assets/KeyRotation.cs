8978068d9bb4c174c9ed07a97981946f700586aa
jcrossley7 <jackcrossley@rocketmail.com>
2019-10-02 16:50:28 +0100

    First commit

---
+++ Assets/KeyRotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyRotation : MonoBehaviour {
+
+    private void Update()
+    {
+        //rotate around local y axis 60 degree per second
+        transform.Rotate(Vector3.down * Time.deltaTime * 60 );
+    }
+}
